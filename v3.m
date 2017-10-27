0f46b447d759bbdfe90aa9a84a29f5d901049990
Jack <xl553@cornell.edu>
2017-10-27 15:05:01 -0400

    Add files via upload
    3D visualization

--- v3.m
+++ v3.m
@@ -1,8 +1,8 @@
-function []= v3 (data)
+function []= v3 (data,fract)
 [d1,d2,d3] = size(data);
-x = floor(d1/3);
-y = floor(d2/3);
-z = floor(d3/3);
+x = floor(d1/fract);
+y = floor(d2/fract);
+z = floor(d3/fract);
 figure('Name','dimension 1 span');
 imagesc(squeeze(data(x,:,:)));
 figure('Name','dimension 3 span');
